c362ccf6ff9213b70eadc198824f6627619ca90e
Will Robinson <will@wambl.com>
2015-03-01 18:40:14 -0800

    PARSE LIBRARY ISSUES #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  require 'ParseAPI'
-
-  # puts "\n\nXXXXXXXXXXXXXXXXXXXXX\n\nXXXXXXXXXXXXXXXXXXXXXXXXX\n\n"
-
   private
 
   def db
